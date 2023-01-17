55b8dca4f2b2bd2660607e013839ca0f04a82459
adunan2 <adunan@wawand.co>
2023-01-17 14:02:56 -0500

    Create, Edit, Delete, View inactive Client

--- cypress/e2e/Basse-Testing/Create_Client.cy.js
+++ cypress/e2e/Basse-Testing/Create_Client.cy.js
@@ -1,4 +1,5 @@
-describe('Create Client', () => {
+//cyp-0004 "Create, Edit, Delete, View inactive Client"
+describe('Client', () => {
     beforeEach("LOGIN", function(){
          cy.visit("http://localhost:3000/auth/login");
          cy.url().should('include','/auth/login')
@@ -32,7 +33,6 @@ describe('Create Client', () => {
         let otherNumber = UserNumber;
         let addressclient = "CRA " + address + " Cll " + address1 + " - " + address2 ;
         let otheraddress = addressclient
-
         cy.get('[href="/admin/clients"]').click();
         cy.get('[href="/admin/clients/new/"]').click();
         cy.get('[id="client-Name"]').type(other);
@@ -42,5 +42,25 @@ describe('Create Client', () => {
         cy.get('[id="add-team-members"]').type('ad v')
         cy.get('li[class="hover:bg-white py-2 px-4"] > .flex > div > .cursor-pointer').click();
         cy.get('[value="Create Client"]').click();
+    })
+    it('Edit Client', function(){
+        cy.get('[href="/admin/clients"]').click();
+        cy.get(':nth-child(1) > .block > .px-4 > .min-w-0.flex > .min-w-0').click();
+        cy.get('[class="bg-white py-2 px-4 mr-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"]').click();
+        cy.get('[id="client-Name"]').type(" edite");
+        cy.get('[class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 cursor-pointer"]').click();
+    })
+    it('Delete Client', function(){
+        cy.get('[href="/admin/clients"]').click();
+        cy.get(':nth-child(1) > .block > .px-4 > .min-w-0.flex > .min-w-0').click();
+        cy.get('[class="bg-red-500 py-2 px-4 mr-2 border border-red-300 rounded-md shadow-sm text-sm font-medium text-white hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"]').click();
+        cy.get('[class="swal2-confirm bg-red-600 w-full rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"]').click();
+    })
+    it('View inactive Client', function(){
+        cy.get('[href="/admin/clients"]').click();
+        cy.get('[class="inline-flex items-center justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"]').click();
+        cy.get('[value="inactive"]').check();
+        cy.get('[class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"]').click();
+        cy.get(':nth-child(1) > .block > .px-4 > .min-w-0.flex > .min-w-0').should('be.visible');
     })
 })
